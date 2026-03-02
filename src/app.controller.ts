cdba17474260130f65124c1738a3779cc6f39013
Sa1i3rii <aomsinwarit@gmail.com>
2026-03-02 19:21:25 +0700

    test09

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+
+import { MissionService } from './mission/mission.service'; 
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  
+  constructor(
+    private readonly appService: AppService,
+    private readonly missionService: MissionService 
+  ) {}
 
   @Get()
   getLove(): string {
     return this.appService.getLove();
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.missionService.remove(id);
   }
 }
